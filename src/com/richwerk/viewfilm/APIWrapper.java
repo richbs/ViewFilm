3cd50d302a5562e33ddb09ea5d869a23325a9503
Richard Barrett-Small <rickbs@gmail.com>
2012-02-16 22:16:53 +0000

    JSON Parsing and response reading

--- src/com/richwerk/viewfilm/APIWrapper.java
+++ src/com/richwerk/viewfilm/APIWrapper.java
@@ -1,5 +1,72 @@
 package com.richwerk.viewfilm;
 
-public class APIWrapper {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.InputStream;
+import android.util.Log;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.HttpClient; 
+
+
+import android.util.Log;
+
+
+public class APIWrapper {
+       
+    public String stringify(InputStream is) throws IOException {
+
+        BufferedReader r = new BufferedReader(new InputStreamReader(is));
+        String x = "";
+        x = r.readLine();
+        String total = "";
+        while(x != null){
+            total += x;
+            x = r.readLine();
+        }
+        Log.v("BAR", total);
+        
+        return total;
+        
+    }
+
+    public void call(String url) {
+
+        HttpClient httpclient = new DefaultHttpClient();
+        
+        // Prepare a request object
+        HttpGet httpget = new HttpGet(url); 
+ 
+        try {
+            HttpResponse response = httpclient.execute(httpget);
+            // Examine the response status
+            Log.v("FOO", response.getStatusLine().toString());
+            HttpEntity ent = response.getEntity();
+            InputStream is = ent.getContent();
+            String enc = "UTF-8";
+            String json_string = this.stringify(is);
+            JSONObject json_o = new JSONObject(json_string);
+            
+            
+            is.close();
+            
+        } catch (ClientProtocolException e1) {
+            // TODO Auto-generated catch block
+            e1.printStackTrace();   
+        } catch (IOException e1) {
+            // TODO Auto-generated catch block
+            e1.printStackTrace();
+        } catch (JSONException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }        
+        
+    }
 }
--- src/com/richwerk/viewfilm/ViewFilmActivity.java
+++ src/com/richwerk/viewfilm/ViewFilmActivity.java
@@ -1,22 +1,8 @@
 package com.richwerk.viewfilm;
 
-import java.io.IOException;
-import android.util.Log;
 import android.app.Activity;
 import android.os.Bundle;
-import org.json.JSONObject;
-import org.apache.http.HttpHost;
-import org.apache.http.HttpRequest;
-import org.apache.http.HttpResponse;
-import org.apache.http.HttpEntity;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpUriRequest;
-import org.apache.http.client.HttpClient; 
-import org.apache.http.conn.ClientConnectionManager;
-import org.apache.http.params.HttpParams;
-import org.apache.http.protocol.HttpContext;
+
 import com.richwerk.viewfilm.APIWrapper;
 
 public class ViewFilmActivity extends Activity {
@@ -25,26 +11,8 @@ public class ViewFilmActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         String url = "http://www.vam.ac.uk/api/json/museumobject/search";
-
-        HttpClient httpclient = new DefaultHttpClient();
- 
-        // Prepare a request object
-        HttpGet httpget = new HttpGet(url); 
- 
-        // Execute the request
-        HttpResponse response;
-
-        try {
-            response = httpclient.execute(httpget);
-            // Examine the response status
-            Log.v("FOO", response.getStatusLine().toString());
-        } catch (ClientProtocolException e1) {
-            // TODO Auto-generated catch block
-            e1.printStackTrace();   
-        } catch (IOException e1) {
-            // TODO Auto-generated catch block
-            e1.printStackTrace();
-        }
+        APIWrapper api = new APIWrapper();
+        api.call(url);
         setContentView(R.layout.main);
     }
 }
